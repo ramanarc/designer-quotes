3f592eb453cac43cd24f3e0e58e5a81847091702
Anuj Raman <anujramanc@gmail.com>
2023-05-30 12:07:43 +0530

    randomized bg colors

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const quoteHeading = document.querySelector("h1");
-const quoteAuthor = document.querySelector('h4');
+const quoteAuthor = document.querySelector('h3');
 const button = document.querySelector('button')
 
 let data = [];
@@ -19,10 +19,22 @@ const getQuote = () => {
 
   // Change headingQuote randomly
   quoteHeading.innerHTML = randomQuote.quote;
-  quoteAuthor.innerHTML = randomQuote.author;
+  quoteAuthor.innerHTML = `— ${randomQuote.author}`;
+}
+
+let colorData = ["#B91C1C", "#C2410C", "#B45309", "#4D7C0F", "#15803D", "#0F766E", "#4338CA", "#4338CA", "#A21CAF", "#3730A3", "#9C0D38", "#586994"]
+
+
+function changeBackgroundColor() {
+  const randomColorNumber = Math.floor(Math.random() * colorData.length)
+
+  let bgColor = colorData[randomColorNumber];
+  document.body.style.backgroundColor = bgColor;
 }
 
 
 button.addEventListener('click', () => {
   getQuote();
+  changeBackgroundColor();
+  // color of body changes to a certain gradient
 });
