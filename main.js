8acc6c082325c305ac0dd856f0d8547f6a0de629
Anuj Raman <anujramanc@gmail.com>
2023-05-30 11:48:09 +0530

    fetching data from api

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const quoteHeading = document.querySelector("h1");
 const quoteAuthor = document.querySelector('h4');
 const button = document.querySelector('button')
-console.log(quoteHeading);
 
-const data = [
-  { quote: "You can have an art experience in front of a Rembrandt… or in front of a piece of graphic design", author: "Dieter Rams" },
-  { quote: "Graphic design will save the world right after rock and roll does.", author: "John Dahlback" },
-  { quote: "It’s through mistakes that you actually can grow. You have to get bad in order to get good.", author: 'Nigahiga' }
-]
+let data = [];
 
-
+fetch("https://api.superhi.com/api/test/quotes")
+  .then(response => response.json())
+  .then(jsonData => {
+    data = jsonData
+    getQuote();
+  });
 
 const getQuote = () => {
   // get a random quote
-  // quoteHeading.innerHTML = 'Randomized Quote here which sounds nice' + Math.random();
   const randomNumber = Math.floor(Math.random() * data.length)
 
   const randomQuote = data[randomNumber];
